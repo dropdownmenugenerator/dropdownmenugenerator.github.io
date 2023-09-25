132d98a55272084b235380d117f4adcdabdb8dd3
DropDownMenuGenerator <143233173+dropdownmenugenerator@users.noreply.github.com>
2023-09-25 21:09:41 +0200

    Add files via upload

---
+++ js/czcionki.js
@@ -0,0 +1,61 @@
+const czcionki = [{
+	n: 'Inherit',
+	f: '"Gupter",serif',
+	h: 'https://fonts.googleapis.com/css2?family=Gupter&display=swap'
+}, {
+	n: 'Abel',
+	f: '"Abel",sans-serif',
+	h: 'https://fonts.googleapis.com/css2?family=Abel&display=swap'
+}, {
+	n: 'Albert',
+	f: '"Albert Sans",sans-serif',
+	h: 'https://fonts.googleapis.com/css2?family=Albert+Sans&display=swap'
+}, {
+	n: 'Brygada 1918',
+	f: '"Brygada 1918",serif',
+	h: 'https://fonts.googleapis.com/css2?family=Brygada+1918&display=swap'
+}, {
+	n: 'Catamaran',
+	f: '"Catamaran",sans-serif',
+	h: 'https://fonts.googleapis.com/css2?family=Catamaran&display=swap'
+}, {
+	n: 'Cormorant',
+	f: '"Cormorant SC",serif',
+	h: 'https://fonts.googleapis.com/css2?family=Cormorant+SC&display=swap'
+}, {
+	n: 'Kanit',
+	f: '"Kanit",sans-serif',
+	h: 'https://fonts.googleapis.com/css2?family=Kanit&display=swap'
+}, {
+	n: 'Merriweather',
+	f: '"Merriweather",serif',
+	h: 'https://fonts.googleapis.com/css2?family=Merriweather&display=swap'
+}, {
+	n: 'Mochiy',
+	f: '"Mochiy Pop One",sans-serif',
+	h: 'https://fonts.googleapis.com/css2?family=Mochiy+Pop+One&display=swap'
+}, {
+	n: 'Rasa',
+	f: '"Rasa",serif',
+	h: 'https://fonts.googleapis.com/css2?family=Rasa&display=swap'
+}, {
+	n: 'Roboto',
+	f: '"Roboto",sans-serif',
+	h: 'https://fonts.googleapis.com/css2?family=Roboto&display=swap'
+}, {
+	n: 'Romanesco',
+	f: '"Romanesco",cursive',
+	h: 'https://fonts.googleapis.com/css2?family=Romanesco&display=swap'
+}, {
+	n: 'Rowdies',
+	f: '"Rowdies",cursive',
+	h: 'https://fonts.googleapis.com/css2?family=Rowdies&display=swap'
+}, {
+	n: 'Shartech',
+	f: '"Share Tech Mono",monospace',
+	h: 'https://fonts.googleapis.com/css2?family=Share+Tech+Mono&display=swap'
+}, {
+	n: 'Yanone',
+	f: '"Yanone Kaffeesatz",sans-serif',
+	h: 'https://fonts.googleapis.com/css2?family=Yanone+Kaffeesatz&display=swap'
+}];
